75bd37fb30d3086d3a3447c523aeef217776bc58
vivianluomin <273104241@qq.com>
2018-10-02 20:43:39 +0800

    终于把磨皮美白的滤镜写出来了

--- app/src/main/assets/normal.sh
+++ app/src/main/assets/normal.sh
@@ -3,14 +3,17 @@ precision mediump float;
 varying vec2 vTextureCoord;
 uniform samplerExternalOES sTexture;
 
+
 void main(){
+
+    vec3 gray = vec3(0.5,0.5,0.5);
     float weight[3];
     weight[0] = float(0.4026);
     weight[1] = float(0.2442);
     weight[2] = float(0.0545);
-    float offset = 0.01;
+    float offset = 1.0/150.0;
    vec2 uv[5],uh[5];
-
+    vec3 centerColor = texture2D(sTexture,vTextureCoord).rgb;
    uv[0] = vTextureCoord;
    uv[1] = vTextureCoord+vec2(0.0,float(-1.0*offset));
    uv[2] = vTextureCoord+vec2(0.0,float(-2.0*offset));
@@ -35,5 +38,18 @@ void main(){
 
     vec3 color = colorh*colorv;
 
-    gl_FragColor = vec4(color,1.0);
+    vec3 high_pass = centerColor-color+gray;
+
+    vec3 cha = vec3(1.0,1.0,1.0);
+
+    vec3 finalColor = (centerColor+high_pass*2.0-cha);
+    finalColor.r = clamp(pow(finalColor.r,0.5),0.0,1.0);
+    finalColor.g = clamp(pow(finalColor.g,0.6),0.0,1.0);
+    finalColor.b = clamp(pow(finalColor.b,0.5),0.0,1.0);
+    vec3 rouguang = 2.0*centerColor*finalColor+centerColor*centerColor-2.0*centerColor*centerColor*finalColor;
+
+    gl_FragColor = vec4( finalColor,1.0);
+    vec3 cc = gl_FragColor.rgb*0.4+rouguang*0.6;
+    gl_FragColor.rgb = cc;
+
 }
